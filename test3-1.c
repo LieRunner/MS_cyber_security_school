ef727036f32dacf436c1a74f2d13e7ea72b13b7b
LieRunner <lenscurse@gmail.com>
2025-08-25 16:16:35 +0900

    ok

---
+++ test3-1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    int x, y;
+    printf("x y A O X N\n");
+    printf("===========\n");
+
+    for (x=0; x<=2; x++) {
+    for (y=0; y<=2; y++) {
+        printf("%d %d\n", x, y);
+    }
+}
+
+    return 0;
+}
