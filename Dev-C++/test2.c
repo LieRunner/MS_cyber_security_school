0c9eacb0747cb7c5bbf92b7c7ef44121ee57b19f
LieRunner <lenscurse@gmail.com>
2025-08-26 10:34:59 +0900

    ok

---
+++ Dev-C++/test2.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+	printf("«’∞Ë  ¿Œµ¶Ω∫\n");
+	int index = 1, val= 0;
+	
+	while(index<10){
+		val= val+index;
+		printf("%3d     %2d\n",val, index); //%(-)(n)d
+		//index++;
+		index = index + 2;
+	}
+}
